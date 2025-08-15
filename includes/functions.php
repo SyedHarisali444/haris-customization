21853c8e6ee94044b57f2fa6c0a7519ac1765ff5
Syed Haris Ali <44765333+SyedHarisali444@users.noreply.github.com>
2025-08-15 20:59:22 +0500

    Day 2 upgraded shortcode with CSS styling + parameters

--- includes/functions.php
+++ includes/functions.php
@@ -5,3 +5,27 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 add_action( 'wp_footer', function() {
     echo "<!-- Haris Customization Loaded -->";
 });
+
+/**
+ * Shortcode: [haris_welcome message="Hello!" color="#0073aa"]
+ */
+function haris_welcome_shortcode( $atts ) {
+    // Default shortcode attributes
+    $atts = shortcode_atts(
+        array(
+            'message' => 'Welcome to my site!',
+            'color'   => '#0073aa', // WP blue
+        ),
+        $atts,
+        'haris_welcome'
+    );
+
+    $date = date_i18n( get_option( 'date_format' ) );
+
+    $html  = "<div class='haris-welcome' style='color: {$atts['color']}; font-weight: bold; font-size: 18px;'>";
+    $html .= esc_html( $atts['message'] ) . " Today is {$date}.";
+    $html .= "</div>";
+
+    return $html;
+}
+add_shortcode( 'haris_welcome', 'haris_welcome_shortcode' );
